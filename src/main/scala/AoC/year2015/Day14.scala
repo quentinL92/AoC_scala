4b143163bbc695cab77fa5c364678b5bd52784ae
Quentin Lutgen <quentin.lutgen@gmail.com>
2021-07-22 22:00:07 +0200

    Day14 Part2 done

--- src/main/scala/AoC/year2015/Day14.scala
+++ src/main/scala/AoC/year2015/Day14.scala
@@ -10,7 +10,7 @@ object Day14 extends AoCDay(2015, 14) {
 
   lazy val ReindeerRegex = """(\w+) can fly (\d+) km/s for (\d+) seconds, but then must rest for (\d+) seconds.""".r
 
-  case class Reindeer(name: String, velocity: Int, duration: Int, breakDuration: Int) {
+  case class Reindeer(name: String, velocity: Int, duration: Int, breakDuration: Int, score: Int = 0) {
     lazy val wholeDuration: Int = duration + breakDuration
   }
 
@@ -19,14 +19,30 @@ object Day14 extends AoCDay(2015, 14) {
       Reindeer(name, velocity.toInt, duration.toInt, breakDuration.toInt)
   }
 
-  lazy val reindeersDistance = reindeers.map { reindeer =>
-    val fullSteps = duration / reindeer.wholeDuration
-    val remainingSeconds = duration % reindeer.wholeDuration
+  lazy val reindeersDistance = reindeers.map(computeDistance(_, duration))
+
+  private def computeDistance(reindeer: Reindeer, iteration: Int): Int = {
+    val fullSteps = iteration / reindeer.wholeDuration
+    val remainingSeconds = iteration % reindeer.wholeDuration
     (
       (fullSteps * reindeer.duration) +
         (if (remainingSeconds > reindeer.duration) reindeer.duration else remainingSeconds)
     ) * reindeer.velocity
   }
 
-  override def resolveDay(): Unit = println(s"The max distance is: ${reindeersDistance.max}")
+  lazy val reindeersScore = (1 to duration).foldLeft(reindeers.map(reindeer => reindeer.name -> 0)) {
+    case (acc, currentSecond) =>
+      val reindeersDistance = reindeers.map(reindeer => reindeer.name -> computeDistance(reindeer, currentSecond))
+      val reindeerBestDistance = reindeersDistance.maxBy(_._2)._2
+      val reindeersWinning = reindeersDistance.filter(_._2 == reindeerBestDistance).map(_._1)
+      acc.map {
+        case (name, score) if reindeersWinning.contains(name) => name -> (score + 1)
+        case other                                            => other
+      }
+  }
+
+  override def resolveDay(): Unit = {
+//    println(s"The max distance is: ${reindeersDistance.max}")
+    println(s"The winning reindeer have: ${reindeersScore.maxBy(_._2)._2} points")
+  }
 }
