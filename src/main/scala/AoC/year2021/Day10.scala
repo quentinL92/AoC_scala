0032dc067feafbda049488a5aaa9788378b1c52c
Quentin Lutgen <quentin.lutgen@gmail.com>
2021-12-10 19:53:58 +0100

    Day 10

---
+++ src/main/scala/AoC/year2021/Day10.scala
@@ -0,0 +1,111 @@
+package AoC.year2021
+
+import AoC.AoCDay
+
+import scala.annotation.tailrec
+import scala.collection.immutable.Queue
+
+object Day10 extends AoCDay(2021, 10) {
+  override def resolveDay(): Unit = {
+    val testMode = false
+
+    lazy val lines = getLines(test = testMode).map(_.trim)
+
+    val errorPoints: Map[Char, Long] = Map(
+      ')' -> 3,
+      ']' -> 57,
+      '}' -> 1197,
+      '>' -> 25137
+    )
+
+    val completionPoints: Map[Char, Long] = Map(
+      ')' -> 1,
+      ']' -> 2,
+      '}' -> 3,
+      '>' -> 4
+    )
+
+    def matchingParenthesis(opening: Char, current: Char): Boolean = (opening, current) match {
+      case '(' -> ')' => true
+      case '[' -> ']' => true
+      case '{' -> '}' => true
+      case '<' -> '>' => true
+      case _          => false
+    }
+
+    def getMatchingClosingParenthesis(opening: Char): Char = opening match {
+      case '(' => ')'
+      case '[' => ']'
+      case '{' => '}'
+      case '<' => '>'
+    }
+
+    def isClosingParenthesis(parenthesis: Char): Boolean =
+      List(')', ']', '}', '>').contains(parenthesis)
+    def isOpeningParenthesis(parenthesis: Char): Boolean =
+      List('(', '[', '{', '<').contains(parenthesis)
+
+    def part1(): Unit = {
+
+      @tailrec
+      def process(remainingElements: List[Char], parenthesisAcc: Queue[Char] = Queue.empty[Char]): Option[Long] = {
+        remainingElements match {
+          case Nil => None
+          case head :: tail if isClosingParenthesis(head) && matchingParenthesis(parenthesisAcc.head, head) =>
+            process(tail, parenthesisAcc.dequeue._2)
+
+          case head :: _ if isClosingParenthesis(head) =>
+            Some(errorPoints.getOrElse(head, 0))
+
+          case head :: tail if isOpeningParenthesis(head) => process(tail, parenthesisAcc.prepended(head))
+          case _ :: tail                                  => process(tail, parenthesisAcc)
+        }
+      }
+
+      val res = lines
+        .map(_.toList)
+        .map(process(_))
+        .collect {
+          case Some(value) => value
+        }
+        .sum
+
+      println(s"Part1 = $res")
+
+    }
+
+    def part2(): Unit = {
+      @tailrec
+      def process(remainingElements: List[Char], parenthesisAcc: Queue[Char] = Queue.empty[Char], score: Long = 0): Option[Long] = {
+        remainingElements match {
+          case Nil if parenthesisAcc.isEmpty =>
+            if (score != 0) Some(score) else None
+
+          case Nil =>
+            val nextClosing = getMatchingClosingParenthesis(parenthesisAcc.head)
+            process(Nil, parenthesisAcc.dequeue._2, score * 5L + completionPoints.getOrElse(nextClosing, 0L))
+
+          case head :: tail if isClosingParenthesis(head) && matchingParenthesis(parenthesisAcc.head, head) =>
+            process(tail, parenthesisAcc.dequeue._2)
+
+          case head :: _ if isClosingParenthesis(head) =>
+            None
+
+          case head :: tail if isOpeningParenthesis(head) => process(tail, parenthesisAcc.prepended(head))
+          case _ :: tail                                  => process(tail, parenthesisAcc)
+        }
+      }
+
+      val res = lines.map(_.toList).map(process(_)).collect {
+        case Some(value) => value
+      }.sorted
+
+      println(res)
+      println(s"Part2 = ${res(res.length / 2)}")
+    }
+
+//    part1()
+    part2()
+
+  }
+}
